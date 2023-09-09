11fffa9f0fa6bb8b830450646b410c984c57f8a7
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-09 20:23:24 -0300

    Update streamlit_ex001.py

--- streamlit_ex001.py
+++ streamlit_ex001.py
@@ -53,3 +53,10 @@ dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 st.table(dataframe)
+
+st.write("Draw a line chart right now:")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
