a217038a59c072d5bc20eca0d7af76cf2a433793
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-09 19:57:05 -0300

    Update streamlit_ex001.py
    C6

--- streamlit_ex001.py
+++ streamlit_ex001.py
@@ -26,11 +26,11 @@ st.write("Ola, ", nome)
 # ... other plotting actions ...
 # st.pyplot(fig)
 
+import streamlit as st
 import pandas as pd
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
-
- 
+}))
