f15a572b704c009e382460296531332e0a095762
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-09 20:01:42 -0300

    Update streamlit_ex001.py

--- streamlit_ex001.py
+++ streamlit_ex001.py
@@ -34,3 +34,9 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+import streamlit as st
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
