8e92b90f9413aa2a79432f43cfa5065a26ab4f5a
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 14:37:55 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -28,9 +28,8 @@ ax.hist(arr, bins=20)
 
 st.pyplot(fig)
 
-# https://docs.streamlit.io/library/api-reference/charts/st.altair_chart
 import altair as alt
-
+# https://docs.streamlit.io/library/api-reference/charts/st.altair_chart
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
@@ -39,6 +38,30 @@ c = alt.Chart(chart_data).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 st.altair_chart(c, use_container_width=True)
+
+
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
+
+chart = alt.Chart(source).mark_circle().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+).interactive()
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Altair theme.
+    st.altair_chart(chart, theme=None, use_container_width=True)
+
+
 
 # https://docs.streamlit.io/library/api-reference/charts/st.vega_lite_chart
 chart_data = pd.DataFrame(
