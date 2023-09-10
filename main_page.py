820d3de037b25db4c73faf3013254f58763ec9ce
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 11:23:20 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -20,12 +20,23 @@ st.bar_chart(
 )
 
 
+import altair as alt
+from vega_datasets import data
 
-import matplotlib.pyplot as plt
+source = data.cars()
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+chart = alt.Chart(source).mark_circle().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+).interactive()
 
-st.pyplot(fig)
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
 
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Altair theme.
+    st.altair_chart(chart, theme=None, use_container_width=True)
