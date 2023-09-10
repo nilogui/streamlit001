bb6c6b1bc4372e71bbd12bbde844d96eff453105
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 15:04:27 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -77,7 +77,26 @@ st.vega_lite_chart(chart_data, {
     },
 })
 
+# B---
+import plotly.figure_factory as ff
 
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+# E---
 
 import plotly.express as px
 import streamlit as st
