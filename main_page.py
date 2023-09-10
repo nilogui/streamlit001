81dea0966a2eb978f7374c65bee0359a251f825a
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 15:32:41 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -177,3 +177,21 @@ graph.edge('sleep', 'runmem')
 
 st.graphviz_chart(graph)
 
+st.graphviz_chart('''
+    digraph {
+        run -> intr
+        intr -> runbl
+        runbl -> run
+        run -> kernel
+        kernel -> zombie
+        kernel -> sleep
+        kernel -> runmem
+        sleep -> swap
+        swap -> runswap
+        runswap -> new
+        runswap -> runmem
+        new -> runmem
+        sleep -> runmem
+    }
+''')
+
