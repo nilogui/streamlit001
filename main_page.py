2cd2d557065b6a47b07b0372feb5197f13b42e05
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 11:17:15 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -3,10 +3,9 @@ import streamlit as st
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
 
-
 import pandas as pd
 import numpy as np
-
+# https://mainpagepy-hmtjm6tcqjuyqe5kbuamnj.streamlit.app/
 chart_data = pd.DataFrame({
     'col1' : np.random.randn(20),
     'col2' : np.random.randn(20),
@@ -19,3 +18,14 @@ st.bar_chart(
     y='col2',
     color='col3'
 )
+
+
+
+import matplotlib.pyplot as plt
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
