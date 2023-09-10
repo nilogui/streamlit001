1e78ec7f70f8a6cd2b58e6f01da227394177e1c1
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 11:44:32 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -31,7 +31,6 @@ c = alt.Chart(chart_data).mark_circle().encode(
 
 st.altair_chart(c, use_container_width=True)
 
-
 # https://docs.streamlit.io/library/api-reference/charts/st.vega_lite_chart
 chart_data = pd.DataFrame(
     np.random.randn(200, 3),
@@ -46,3 +45,25 @@ st.vega_lite_chart(chart_data, {
         'color': {'field': 'c', 'type': 'quantitative'},
     },
 })
+
+# https://docs.streamlit.io/library/api-reference/charts/st.plotly_chart
+import plotly.figure_factory as ff
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+
