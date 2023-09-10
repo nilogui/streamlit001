16c891587e1f581eb9511acd6c089fe8a2837567
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-09 21:55:59 -0300

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -29,7 +29,8 @@ st.write(data)
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+st.bar_chart(hist_values) # Streamlit supports more complex charting libraries like Altair, Bokeh, Plotly, Matplotlib and more.
 
-
+st.subheader('Map of all pickups')
+st.map(data)
 
