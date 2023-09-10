f8d59f87f2ba0bfeb81063fbac93a8bd478aee16
nilogui <34976555+nilogui@users.noreply.github.com>
2023-09-10 11:14:19 -0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,3 +2,20 @@ import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame({
+    'col1' : np.random.randn(20),
+    'col2' : np.random.randn(20),
+    'col3' : np.random.choice(['A','B','C'],20)
+})
+
+st.bar_chart(
+    chart_data,
+    x='col1',
+    y='col2',
+    color='col3'
+)
